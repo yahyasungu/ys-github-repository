1ad5e0a00f44d2c27d12fc5d92fc4a4a15f60230
yahyasungu <yahyasungu@Yahyas-MacBook-Pro.local>
2018-05-07 10:10:30 +0300

    change the port

---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+ * Primary file for API
+ *
+ */
+
+// Dependencies
+var http = require('http');
+var url = require('url');
+var port = 3001;
+
+ // Configure the server to respond to all requests with a string
+var server = http.createServer(function(req,res){
+
+  // Parse the url
+  var parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Send the response
+  res.end('Hello github World!\n');
+
+  // Log the request/response
+  console.log('Request received on path: '+trimmedPath);
+});
+
+// Start the server
+server.listen(port,function(){
+  console.log('The server is up and running now');
+});
